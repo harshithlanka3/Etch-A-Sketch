522d7182c1bdf6768ea499f17fe99cef1d1e04b3
Harshith Lanka <98058276+harshithlanka3@users.noreply.github.com>
2022-12-24 01:49:39 +0530

    Added size changing and clear functionality - Added a slider that allows the user to change the size of the grid from 16-100 - Added a clear button that resets the grid to the current size - Added necessary functions in the script.js file to make features above work - Added styling to the slider and the clear button ! Need to work on color functionality of the app ? Maybe add a way to save a user's drawing in the far future

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const DEFAULTSIZE = 16;
 
 let size = DEFAULTSIZE;
+let isDrawing = false;
 
 const grid = document.querySelector('.grid');
+const sizeInput = document.querySelector('.size-input');
+const sizeDisplay = document.querySelector('.size-display');
+const clear = document.querySelector('.clear');
 
-let isDrawing = false;
+sizeDisplay.textContent = `Size: ${DEFAULTSIZE}x${DEFAULTSIZE}`;
+
+sizeInput.oninput = function() {
+    size = this.value;
+    sizeDisplay.innerHTML = `Size: ${size}x${size}`;
+    reloadGrid();
+}
+
 grid.addEventListener('click', () => {
     isDrawing = !isDrawing;
 });
+
+clear.addEventListener('click', resetGrid);
+
+function resetGrid() {
+    clearGrid();
+    createGrid(size);
+}
+
+function reloadGrid() {
+    clearGrid();
+    createGrid(size);
+}
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
 
 function createGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
